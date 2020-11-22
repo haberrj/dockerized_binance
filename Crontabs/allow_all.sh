d11db044630c0731352e522282c4cb42948287a3
Ron Haber <haberrj95@gmail.com>
2020-11-22 19:58:39 +0100

    Added allow_all script for permissions

---
+++ Crontabs/allow_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Ron Haber
+# Will activate the scripts so they have permission to run
+
+chmod +x /dockerized_binance/Crontabs/binance_thresholds.sh
+chmod +x /dockerized_binance/Crontabs/real_binance_trading.sh
