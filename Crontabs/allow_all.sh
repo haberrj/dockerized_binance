2e86980a2445e2637d4154b1b3e3a817c45572ba
Ron Haber <haberrj95@gmail.com>
2020-11-22 01:33:28 +0100

    Updated Cronjobs and docker file

---
+++ Crontabs/allow_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Ron Haber
+# This script will set the scripts to be executable
+
+chmod +x /dockerized_binance/Crontabs/binance_thresholds.sh
+chmod +x /dockerized_binance/Crontabs/real_binance_trading.sh
