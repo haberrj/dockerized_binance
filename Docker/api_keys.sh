552e031ec13737e5ae017e4c14058137192cd614
Ron Haber <haberrj95@gmail.com>
2020-11-30 16:58:40 +0100

    Appended some commands to the docker file to run forever and except the API values

---
+++ Docker/api_keys.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Author: Ron Haber
+# Will set the file for API Keys for use with Binance
+
+public=$1
+secret=$2
+
+mkdir /bin/api_keys
+
+cd /bin/api_keys/
+touch API_Binance_public_key
+touch API_Binance_secret_key
+
+public_direc="/bin/api_keys/API_Binance_public_key"
+secret_direc="/bin/api_keys/API_Binance_secret_key"
+
+if [ -f "$public_direc" ]
+then 
+    echo "$public" > "$public_direc"
+fi
+
+if [ -f "$secret_direc" ]
+then 
+    echo "$secret" > "$secret_direc"
+fi
+
+while true # This is an infinite loop to keep the docker up
+do
+    sleep 1
+done
