cd1adccbfeb586e82006c215e97dd932943d7cb0
Ron Haber <haberrj95@gmail.com>
2020-12-06 19:14:04 +0100

    Added some prelim start up scripts

--- Startup_Scripts/setup.sh
+++ Startup_Scripts/setup.sh
@@ -1,1 +1,12 @@
 # This script will install required dependencies
+# Script must be run with sudo
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with elevated permission rights"
+  exit
+fi
+
+apt-get update
+apt-get install -y python-dev python3-pip docker
+
+# Get the git repos here
